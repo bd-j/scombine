a38b2fff3df9d3354e2710ae1fa26cf873f5e2f4
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-08-24 13:31:53 -0700

    method for imf

--- imfvariance.py
+++ imfvariance.py
@@ -4,10 +4,13 @@ import fsps
 
 sps = fsps.StellarPopulation()
 
-if __name__ == '__main__':
 
-    imf0, var = 2.7, 0.7
-    dalpha = 0.1
+def effective_imf(imf0=2.7, var=0.7, dalpha=0.05):
+    
+    
+
+def sps_varimf(imf0=2.7, var=0.7, dalpha=0.05, verbose=False):
+
     sps.params['sfh'] = 1 #five parameter sfh
     sps.params['const'] = 1 #everything in the constant
     sps.params['imf_type'] = 2 # Kroupa 3-slope IMF
@@ -16,22 +19,28 @@ if __name__ == '__main__':
     weights = 1./(np.sqrt(var*2.*np.pi)) * np.exp( -(imfs - imf0)**2/(2*var))
     
     spec = np.zeros([ len(sps.wavelengths), len(imfs) ])
+    mass = np.zeros(len(imfs)) 
     for i, imf in enumerate(imfs):
         sps.params['imf3'] = imf
         wave, ispec = sps.get_spectrum(peraa=True, tage=1)
-        logmass = sps.stellar_mass
+        mass[i] = sps.stellar_mass
         spec[:,i] = ispec
-    sumspec = (spec * weights[None, :]).sum(-1) * dalpha
+    norm = (weights*dalpha).sum()
+    sumspec = (spec * (weights)[None, :]).sum(-1) * dalpha
+    if verbose: print(norm)
+    
     ind = np.argmin(np.abs(imfs-imf0))
     ind_salp = np.argmin(np.abs(imfs-2.3))
     
     fig, ax = pl.subplots(1,1)
     ax.plot(wave, sumspec/spec[:,ind], label = r'$L_\lambda(\alpha=vimf)/L_\lambda(\alpha=2.7) \, \times \, C_1$')
     ax.plot(wave, sumspec/spec[:,ind_salp], label = r'$L_{{\lambda}}(\alpha=vimf)/L_\lambda(\alpha=2.3) \, \times \, C_2$')
-    ax.set_xlim(1e3,1e4)
+    ax.set_xlim(1e2,1e4)
     ax.set_ylim(0.5, 2.5)
     ax.legend(loc=0)
-    fig.show()
+    return fig, ax, [imfs, spec, mass, weights, sumspec]
 
     
-
+if __name__ == '__main__':
+    fig, ax, blob = sps.varimf()
+    fig.show()
