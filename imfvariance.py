2f64b1ea537dadc94488215cd3a970e860dbfe02
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-08-27 15:06:50 -0700

    working effective imf code

--- imfvariance.py
+++ imfvariance.py
@@ -1,41 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as pl
 import fsps
+from scipy.integrate import romberg as qromb
 
 sps = fsps.StellarPopulation()
 
-def effective_imf(masses, imf0=2.7, var=0.7, dalpha=0.05):
-    """
-    Work out the effective (slope averaged) imf in the case of
-    variable upper slopes.
-    """
-    
-    imfs = np.arange(0.1, 5.4, dalpha)
-    weights = 1./(np.sqrt(var*2.*np.pi)) * np.exp( -(imfs - imf0)**2/(2*var))
-    for imf in imfs:
-        phi = kroupa(masses, imf3=imf)
-        
-def kroupa(masses, imf0=0.3, imf1=1.3, imf2=2.3, imf3=2.3,
+
+def kroupa(mass, imf0=0.3, imf1=1.3, imf2=2.3, imf3=2.3,
            imf_lower_limit=0.08, imf_upper_limit=100):
     """
     Implements a Kroupa (2001) IMF, normalized to 1 M_sun
     """
-    
+    mass = np.atleast_1d(mass)
     alphas = np.array([imf0, imf1, imf2, imf3])
     lims = [imf_lower_limit, 0.08, 0.5, 1.0, imf_upper_limit]
     #lims = [0.08, 0.5, 1.0]
     #alpha_ind = np.digitize(masses, lims)
     #phi = masses**(-alphas[alpha_ind])
 
-    phi = np.zeros_like(masses)
+    phi = np.zeros_like(mass)
     norm = 0.0
     for i, alpha in enumerate(alphas):
-        this = (masses > lims[i]) & (masses < lims[i+1])
-        phi[this] = masses[this]**(-alpha)
+        this = (mass > lims[i]) & (mass < lims[i+1])
+        if i == 0:
+            n = 1.0
+        else:
+            n *= lims[i]**(alpha - alphas[i-1])
+        phi[this] = mass[this]**(-alpha) * n
         ex = (2.0-alpha)
-        norm += (lims[i+1]**ex - lims[i]**ex)/ex
-
+        norm += (lims[i+1]**ex - lims[i]**ex) * n/ex
     return phi/norm
+
+def effective_imf(masses, imf0=2.7, var=0.7, dalpha=0.05, function=kroupa):
+    """
+    Work out the effective (slope averaged) imf in the case of
+    variable upper slopes.
+    """
+    phi = np.zeros_like(masses)
+    imfs = np.arange(0.1, 5.4, dalpha)
+    weights = 1./(np.sqrt(var*2.*np.pi)) * np.exp( -(imfs - imf0)**2/(2*var)) * dalpha
+    for w, imf in zip(weights, imfs):
+        phi += w * function(masses, imf3=imf)
+    return phi
 
 def sps_varimf(imf0=2.7, var=0.7, dalpha=0.05, verbose=False):
     """
@@ -81,7 +87,7 @@ def sps_varimf(imf0=2.7, var=0.7, dalpha=0.05, verbose=False):
 
     return [sumspec, imfs, weights, spec, mass]
         
-def plot_varimf(sumspec, spec, imfs, df_imf_slopes = [2.7, 2.35])
+def plot_varimf(sumspec, spec, imfs, df_imf_slopes = [2.7, 2.35]):
     """
     Plot the integrated spectrum from the variable imf divided by the
     integrated spectrum from \delta-function imf slopes.
@@ -128,5 +134,18 @@ if __name__ == '__main__':
     
     imf0, var = 2.7, 0.7
     blob = sps_varimf(imf0=imf0, var=var)
-    fig, ax = plot_varimf(blob[0], blob[3], df_imf_slops=[imf0, 2.35])
+    fig, ax = plot_varimf(blob[0], blob[3], blob[1], df_imf_slopes=[imf0, 2.3])
+    fig.show()
+
+    fig, ax = pl.subplots(1,1)
+    masses = np.arange(0.08,100.,0.001)
+    phi = kroupa(masses, imf3=2.3)
+    phi_var = effective_imf(masses, imf0=imf0, var=var, function=kroupa)
+    ax.plot(masses, phi, label = r'Kroupa (2001)')
+    ax.plot(masses, phi_var, label = r'Variable IMF ($\mu={0}, \sigma^2={1}$)'.format(imf0,var))
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('$m$')
+    ax.set_ylabel(r'$N(m)dm$')
+    ax.legend(loc=0)
     fig.show()
